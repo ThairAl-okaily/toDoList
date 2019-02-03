ab9a4d4c2f0bb246b305b3eff57308a9ffd42950
thair alokaily <thairalokaily@gmail.com>
2019-02-03 12:49:28 -0800

    fix some bugs and add new functionulity

---
+++ assets/js/todos.js
@@ -0,0 +1,29 @@
+
+$(document).ready(function(){
+//check of specific by clicking 
+$("ul").on("click", "li", function () { 
+   $(this).toggleClass("don");
+});
+});
+
+$(document).ready(function(){
+ //click on the X span will delet the todo 
+ $("ul").on("click", "span", function (e) { 
+     e.stopPropagation();
+     $(this).parent().fadeOut(500,function () {
+         $(this).remove();
+     });
+ });
+});
+
+$(document).ready(function(){
+$("#addToDo").keypress(function(e){
+    if(e.which === 13){
+        let todotxt = $(this).val();
+        //add li and hook the .text();
+        $("ul").append("<li><span>X </span>" + todotxt + "</li>");
+        $(this).val("");
+    }
+});
+});
+
